b798f42a2cd527f51e554b5291201e46da959372
Sergio I. Urbina <checor@gmail.com>
2015-01-17 15:05:14 -0600

    Primeros pasos

---
+++ uninstall.php
@@ -0,0 +1,4 @@
+<?php
+/*Placeholder of best practices, there is nothing here right now*/
+?>
+
--- woocommerce-importador.php
+++ woocommerce-importador.php
@@ -3,9 +3,8 @@
 Plugin Name: Woocommerce Price Updater
 Description: Work-in-progress plugin that get prices from Amazon and other sites, and updates the prices accordingly, calculating final prices in the local currency.
 Author: Sergio I Urbina
-License: WTFPL
+License: GPLv2
  */
-
 
 //Dashboard widget                                                           .
 
@@ -13,16 +12,16 @@ add_action( 'wp_dashboard_setup', 'che_dashboard_widget' );
 
 function che_dashboard_widget () {
 	wp_add_dashboard_widget('che_dashboard', 'Price updater info', 
-		'che_dashboard_body');
-}
+			'che_dashboard_body');
+	}
 
-$cero = 0;
 
-function che_dashboard_body() {
-	echo '<p>Ultima actualizacion: n/a</p>';
-	echo '<p>Precios aumentados:n/a \n</p>';
-	echo '<p>Precios reducidos: $cero \ntest</p>';
-}
+	function che_dashboard_body() {
+		$foo = "orcirtap";
+		echo '<p>Ultima actualizacion: n/a</p>';
+		echo '<p>Precios aumentados:n/a \n</p>';
+		echo "<p>Precios reducidos: $foo \ntest</p>";
+	}
 
 //Metabox widget
 
@@ -30,11 +29,11 @@ add_action( 'add_meta_boxes', 'che_metabox_create');
 
 function che_metabox_create() {
 	add_meta_box('che_meta', 'Price updater', 'che_metabox_body', 'product',
-		'normal', 'deafult');
+		'normal', 'high');
 }
 
 function che_metabox_body() {
-	echo 'Opciones: TODO';
+	echo 'Hola Guaymo!';
 }
 
 //Top level menu
